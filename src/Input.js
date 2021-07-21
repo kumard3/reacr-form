df7439e1c9bbc8cf62af0f89546af8398da5d39c
kumard3 <kumardeepanshu157200@gmail.com>
2021-07-21 21:56:17 +0530

    input field added

---
+++ src/Input.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export const Input = () => {
+    return (
+        <div className="input">
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+        <input /> <button> submit</button>
+      </div>
+    )
+}
